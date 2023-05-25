8a14dd22cde30e41dca5560b25c1444282415e52
Riftland <fran.mscr@gmail.com>
2023-05-25 13:26:07 +0200

    feat: created list component

---
+++ src/components/List/styles.jsx
@@ -0,0 +1,13 @@
+import { styled } from "styled-components";
+
+const Wrapper = styled.section`
+  height: 7rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 3rem;
+`;
+
+export default {
+  Wrapper,
+};
