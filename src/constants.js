3c33f443c62ab0ec8d81deadd8214e3967ddcb82
Riftland <fran.mscr@gmail.com>
2023-05-25 13:27:48 +0200

    chore: created a list of themes as constants

---
+++ src/constants.js
@@ -0,0 +1,19 @@
+export const THEMES = {
+  DEFAULT: {
+    fonts: "#C6D2ED",
+    background: "#827081",
+    border: "#E3D0D8",
+  },
+  JUNGLE: {
+    fonts: "#51D6FF",
+    background: "#A06B9A",
+    border: "#522B29",
+  },
+  CHARLYNE: {
+    fonts: "#F7A072",
+    background: "#EDDEA4",
+    border: "#00A7E1",
+  },
+};
+
+export const THEME_OPTIONS = ["DEFAULT", "JUNGLE", "CHARLYNE"];
